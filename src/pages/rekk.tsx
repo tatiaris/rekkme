cde47dc9c0f4ee5313b9007e066ee001500eae7e
tatiaris <tatiaris2001@gmail.com>
2022-01-29 17:12:39 -0600

    teting

--- src/pages/rekk.tsx
+++ src/pages/rekk.tsx
@@ -2,16 +2,28 @@ import React, { useEffect, useState } from 'react';
 
 const Rekk = (): React.ReactNode => {
   const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+  const [url, setUrl] = useState('');
 
   useEffect(() => {
     if (window) {
       const queryParams = new URLSearchParams(window.location.search);
       const title = queryParams.get('title');
+      const text = queryParams.get('text');
+      const url = queryParams.get('url');
       setTitle(title);
+      setText(text);
+      setUrl(url);
+    } else {
+      setTitle('Rekk');
     }
   }, []);
 
-  return <>Rekk Page: {title}</>;
+  return (
+    <>
+      Rekk Page: {title}, {text}, {url}
+    </>
+  );
 };
 
 export default Rekk;
