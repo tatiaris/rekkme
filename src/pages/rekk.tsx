20ed4786a293569d14f221713424f675c1ee358e
tatiaris <tatiaris2001@gmail.com>
2022-01-29 16:36:19 -0600

    pls

--- src/pages/rekk.tsx
+++ src/pages/rekk.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Rekk = (): React.ReactNode => {
-  const queryParams = new URLSearchParams(window.location.search);
-  const title = queryParams.get('title');
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    if (window) {
+      const queryParams = new URLSearchParams(window.location.search);
+      const title = queryParams.get('title');
+      setTitle(title);
+    }
+  }, []);
 
   return <>Rekk Page: {title}</>;
 };
