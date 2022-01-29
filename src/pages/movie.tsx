e53f127cdf60ca57bd63580784f43e48ab4620c4
tatiaris <tatiaris2001@gmail.com>
2022-01-29 13:31:52 -0600

    css fix

--- src/pages/movie.tsx
+++ src/pages/movie.tsx
@@ -4,34 +4,30 @@ import StringInput from '../components/ui/StringInput';
 
 const Movie = (): React.ReactNode => {
   const [movies, setMovies] = useState([]);
-const [movieName, setMovieName] = useState('');
+  const [movieName, setMovieName] = useState('');
 
-async function getMovie() {
-  console.log('hello')
-  const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env("TMDB_API_KEY")}&query=` + movieName, fetch);
-  const data = await res.json()
-  // if (error) return <div>failed to load</div>;
-  // if (!data) return <div>loading...</div>;
-  const newMovies = [];
-  console.log(data)
+  async function getMovie() {
+    console.log('hello');
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env('TMDB_API_KEY')}&query=` + movieName, fetch);
+    const data = await res.json();
+    // if (error) return <div>failed to load</div>;
+    // if (!data) return <div>loading...</div>;
+    const newMovies = [];
+    console.log(data);
 
     data.results.forEach((movie) => {
       newMovies.push({
-          title: movie?.title || movie?.original_title || 'No Title Found',
-          description: movie?.overview || 'No Description Found',
+        title: movie?.title || movie?.original_title || 'No Title Found',
+        description: movie?.overview || 'No Description Found'
       });
     });
-  setMovies(newMovies);
+    setMovies(newMovies);
   }
   return (
     <>
       <StringInput config={{ name: 'name', label: 'Name', type: 'text', placeholder: 'Rick & Morty' }} updateFunc={setMovieName} />
-      <div>
-      {movies}
-    </div>
-    <button onClick={getMovie}>
-  Activate Lasers
-</button>
+      <div>{movies}</div>
+      <button onClick={getMovie}>Activate Lasers</button>
     </>
   );
 };
