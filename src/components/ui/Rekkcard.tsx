b03f65cc800a23369873f27e8b7cf453963f2511
tatiaris <tatiaris2001@gmail.com>
2022-01-29 14:43:01 -0600

    card ui update

--- src/components/ui/Rekkcard.tsx
+++ src/components/ui/Rekkcard.tsx
@@ -16,24 +16,22 @@ export const RekkCard: React.FC<RekkCardProps> = (props) => {
           <img width="40px" src={content.fromImage} alt="" style={{ borderRadius: '50%' }} />
         </div>
         <div style={{ width: '100%', textAlign: 'left', padding: '0 10px' }}>
-          <span>
+          <span style={{ fontSize: '20px' }}>
             <b>{content.from}</b>
-          </span>
-          {` `}to{` `}
-          <span>
+            {` `}to{` `}
             <b>{content.to}</b>
           </span>
           <br />
-          <span style={{ fontSize: '14px' }}>{content.date}</span>
-          <br />
-          <div style={{ height: '5px' }}></div>
+          <div style={{ height: '10px' }}></div>
           <span>
             <b>{content.title}</b> by <b>{content.artist}</b>
           </span>
           <br />
           <span style={{ fontStyle: 'italic', fontSize: '14px' }}>{content.description}</span>
         </div>
-        <div>
+        <div style={{ textAlign: 'right' }}>
+          <span style={{ fontSize: '14px' }}>{content.date}</span>
+          <div style={{ height: '5px' }}></div>
           <img width="80px" src={content.image} alt="" />
         </div>
       </div>
