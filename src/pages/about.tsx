b27ba51a0ccd6c95cdaba4d8c0e39900b625a8de
tatiaris <tatiaris2001@gmail.com>
2022-01-30 10:57:23 -0600

    pls

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,12 +10,7 @@ const About = (): React.ReactNode => (
         <b>Motivation</b>
       </span>
       <br />
-      <div
-        style={{
-          fontStyle: 'normal',
-          fontSize: '20px'
-        }}
-      >
+      <div style={{ fontStyle: 'normal', fontSize: '20px' }}>
         {`Have you ever gotten tired of skimming through Netflix, looking for something to watch? Or visited a new city and had to search hundreds of texts to find a restaurant that was highly recommended by a friend? Maybe you've found the perfect album for a friend, but know they'll forget to listen if they're not reminded.`}
       </div>
       <div style={{ height: '30px' }}></div>
@@ -23,12 +18,9 @@ const About = (): React.ReactNode => (
         <b>Mission</b>
       </span>
       <br />
-      <div
-        style={{
-          fontStyle: 'normal',
-          fontSize: '20px'
-        }}
-      >{`Rekkme is a personalized recommendation app that makes it easy to share recommendations and compete with your friends to see who has the best taste!`}</div>
+      <div style={{ fontStyle: 'normal', fontSize: '20px' }}>
+        Rekkme is a personalized recommendation app that makes it easy to share recommendations and compete with your friends to see who has the best taste!
+      </div>
     </div>
   </>
 );
