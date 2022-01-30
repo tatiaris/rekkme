68f9c440d26cc930e92a2ea40b629a7c18d4154f
tatiaris <tatiaris2001@gmail.com>
2022-01-29 22:55:25 -0600

    pls

--- src/pages/discover.tsx
+++ src/pages/discover.tsx
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Navbar from '../components/common/Navbar';
+import { circularText } from '../components/Helper';
 
-const Discover = (): React.ReactNode => {
-  return <></>;
+const Discover = (props): React.ReactNode => {
+  useEffect(() => {
+    if (document) {
+      circularText('MUSIC', 100, 0);
+      circularText('MOVIES', 100, 1);
+      circularText('BOOKS', 100, 2);
+      circularText('PRODUCTS', 100, 3);
+      circularText('BITES', 100, 4);
+      circularText('RECIPES', 100, 5);
+      circularText('TRAVEL', 100, 6);
+    }
+  }, []);
+
+  return (
+    <>
+      <Navbar userSession={props.userSession} pageTitle="Discover" />
+      <div className="discover-container">
+        <div className="category-container">
+          <a href="/discover/music">
+            <div className="circTxt">
+              <img width="150" src="/badges/MusicBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+        <div className="category-container">
+          <a href="/discover/tv">
+            <div className="circTxt">
+              <img width="150" src="/badges/TVBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+        <div className="category-container">
+          <a href="/discover/books">
+            <div className="circTxt">
+              <img width="150" src="/badges/BookBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+        <div className="category-container">
+          <a href="/discover/products">
+            <div className="circTxt">
+              <img width="150" src="/badges/ProductBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+        <div className="category-container">
+          <a href="/discover/restaurants">
+            <div className="circTxt">
+              <img width="150" src="/badges/RestaurantBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+        <div className="category-container">
+          <a href="/discover/recipes">
+            <div className="circTxt">
+              <img width="150" src="/badges/RecipeBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+        <div className="category-container">
+          <a href="/discover/travel">
+            <div className="circTxt">
+              <img width="150" src="/badges/TravelBadge.png" alt="" />
+            </div>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Discover;
