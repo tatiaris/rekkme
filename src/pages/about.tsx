7f1c90b7f6d909fb8f3af59e53aca4389387777e
Dan Abreo <dabreo43@gmail.com>
2022-01-30 07:16:09 -0600

    css

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ const About = (): React.ReactNode => {
     <>
       <Navbar userSession={null} pageTitle="About" />
       <img style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }} width="220" height="110" src="/logos/Rekkme_LinearLogoLM.svg" alt="logo" />
-      <div style={{ width: '100%', textAlign: 'left', padding: '10px 35px' }}>
+      <div style={{ textAlign: 'left', padding: '35px' }}>
         <span style={{ color: '#AE919A', fontSize: '25px' }}>
           <b>Mission</b>
         </span>
@@ -14,8 +14,7 @@ const About = (): React.ReactNode => {
         <div
           style={{
             fontStyle: 'normal',
-            fontSize: '20px',
-            padding: '0 65px 0 0'
+            fontSize: '20px'
           }}>
           {`Have you ever opened up Netflix and wondered what to watch, or visited a new city and had to scroll through hundreds of text messages to find a restaurant that was highly recommended by a friend?`}
         </div>
@@ -27,8 +26,7 @@ const About = (): React.ReactNode => {
         <div
           style={{
             fontStyle: 'normal',
-            fontSize: '20px',
-            padding: '0 65px 0 0'
+            fontSize: '20px'
           }}>{`Rekkme is a convenient way to share personalized recommendations with friends and family, and prove who has the best taste in pretty much anything!`}</div>
       </div>
     </>
