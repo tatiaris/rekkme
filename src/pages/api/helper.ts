d993fdeaa3171f7d4ebfe4cfdcf669fc444bbd72
Rishabh <tatiaris2001@gmail.com>
2022-02-06 01:48:41 -0600

    more cleanup

--- src/pages/api/helper.ts
+++
@@ -1,9 +0,0 @@
-export const getErrorStatusCode = (code) => {
-  try {
-    const status = parseInt(code);
-    if (status) return status;
-    else throw new Error('500');
-  } catch (error) {
-    return 500;
-  }
-};
